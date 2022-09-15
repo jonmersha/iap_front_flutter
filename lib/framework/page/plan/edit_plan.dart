3bf8a39ceca5274f915554fccbf2e83f54347fb8
jonmersha@gmail.com <jonmersha@gmail.com>
2022-09-15 05:32:42 +0300

    update checking

--- lib/framework/page/plan/edit_plan.dart
+++ lib/framework/page/plan/edit_plan.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iap/framework/data/model/annual_plan_model.dart';
-
 import '../../../menu/controllers.dart';
+
+
 
 class EditAnnualPlan extends StatelessWidget {
   final AuditPlan auditPlan;
    EditAnnualPlan({Key? key, required this.auditPlan}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-
         Column(
           children: [
             Text(auditPlan.auditName!),
             Text(auditPlan.engmonth!.toString()),
-
-
           ],
         ),
         ElevatedButton.icon(onPressed: (){gotoV();}, icon: Icon(Icons.keyboard_backspace_outlined), label: const Text("Back")),
@@ -26,6 +23,7 @@ class EditAnnualPlan extends StatelessWidget {
     );
   }
 }
+
 
 void gotoV(){
   Get.back();
